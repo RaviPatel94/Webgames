a57673d758583684b1be0997e9d27433304762b1
Ravi Patel <139551180+RaviPatel94@users.noreply.github.com>
2024-12-27 22:00:19 +0530

    upadate rules

--- src/games/Matchthechessman.jsx
+++ src/games/Matchthechessman.jsx
@@ -442,7 +442,7 @@ const createChessMatchingGrid = (tiles) => {
       <div className={' absolute left-0 top-11 right-0 mx-auto flex-col items-center justify-center bg-lightgrey py-10 px-8 z-40 max-h-screen w-max h-max btnnohover '+ (popup?"":"hidden")}>
       <div className='h-[450px] sm:h-96 w-[280px] sm:w-[500px] flex flex-col gap-7 items-center justify-center font-medium text-xl sm:text-2xl'>
         <h2 className=' text-4xl'>How to play?</h2>
-        <p className='w-ful'>In this strategic board game, your goal is to match chess pieces by predicting their positions. Each piece must be able to reach its pair’s position on the board—but here’s the twist: Pawns are the bad guys! Matching pawns will cost you some points, so stay sharp and avoid them. Think smart, play strategically, and try to match the pieces in as few tries as possible. Score above 12 points and victory is yours!</p>
+        <p className='w-ful'>In this strategic board game, your goal is to match chess pieces by predicting their positions. Each piece must be able to reach its pair’s position even if there are peices in between- but here’s the twist: Pawns are the bad guys! Matching pawns will cost you some points, so stay sharp and avoid them. Think smart, play strategically, and try to match the pieces in as few tries as possible. Score above 12 points and victory is yours!</p>
         <Btn text="Got it" ClickEvent={removepopup}/>
         </div>
       </div>
@@ -493,7 +493,7 @@ const createChessMatchingGrid = (tiles) => {
         <div className='flex flex-col gap-7 items-center pb-8 sm:pb-0'>
           <h1 className='text-4xl text-center font-medium'>Match The Chessman</h1>
           <p className='text-2xl font-medium lg:w-[600px] pb-7 lg:pb-0 px-5 hidden sm:block'>
-          Are you ready for a unique chess-inspired challenge? In this strategic board game, your goal is to match chess pieces by predicting their positions. Each piece must be able to reach its pair’s position on the board—but here’s the twist: Pawns are the bad guys! Matching pawns will cost you some points, so stay sharp and avoid them. Think smart, play strategically, and try to match the pieces in as few tries as possible. Score above 12 points and victory is yours! Good luck and have fun!
+          Are you ready for a unique chess-inspired challenge? In this strategic board game, your goal is to match chess pieces by predicting their positions. Each piece must be able to reach its pair’s position even if there are peices in between- but here’s the twist: Pawns are the bad guys! Matching pawns will cost you some points, so stay sharp and avoid them. Think smart, play strategically, and try to match the pieces in as few tries as possible. Score above 12 points and victory is yours! Good luck and have fun!
           </p>
           <div className='sm:hidden flex justify-between items-center w-full pr-4'>
           <Btn text="Guide?" ClickEvent={help}/>
